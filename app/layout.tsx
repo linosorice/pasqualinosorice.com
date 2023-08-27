97ef36cf84634ff2fb79c3757bdd0f28571efd33
Pasqualino Sorice <lino.sorice@gmail.com>
2023-08-27 23:38:59 +0200

    fix google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import clsx from 'clsx'
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import Sidebar from './components/sidebar'
-import { Suspense } from 'react'
-import Analytics from './components/analytics'
+import Script from 'next/script'
+import { GTM_ID } from 'lib/gtm'
 
 const graphik = localFont({
   src: [
@@ -67,13 +67,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       lang='en'
       className={clsx('text-black bg-white dark:text-white dark:bg-[#111010]', graphik.variable)}
     >
+      <Script id='google-tag-manager' strategy='afterInteractive'>
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body className='antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto'>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`
+          }}
+        />
         <main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
           <Sidebar />
           {children}
-          <Suspense>
-            <Analytics />
-          </Suspense>
         </main>
       </body>
     </html>
