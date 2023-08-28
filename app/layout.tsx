c93002e9b58bac0fa564fc0543c88f402d0091f6
Pasqualino Sorice <lino.sorice@gmail.com>
2023-08-28 15:06:47 +0200

    fix analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,8 @@ import Sidebar from './components/sidebar'
 import Script from 'next/script'
 import { GTM_ID, pageview } from 'lib/gtm'
 import { usePathname, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
+import { Suspense, useEffect } from 'react'
+import Analytics from './components/analytics'
 
 const graphik = localFont({
   src: [
@@ -79,20 +80,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={clsx('text-black bg-white dark:text-white dark:bg-[#111010]', graphik.variable)}
     >
       <body className='antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto'>
-        <Script id='google-tag-manager' strategy='afterInteractive'>
-          {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-        </Script>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`
-          }}
-        />
+        <Suspense>
+          <Analytics />
+        </Suspense>
         <main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
           <Sidebar />
           {children}
